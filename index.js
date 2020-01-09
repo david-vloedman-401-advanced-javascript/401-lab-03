6c7980f1156e65561340ac7b4b91c285357e2c5d
David Vloedman <djvloedman@gmail.com>
2020-01-08 16:36:43 -0800

    Created edit-file.js with read/write

---
+++ index.js
@@ -0,0 +1,44 @@
+const validator = require('./lib/validatorClass');
+const file = require('./edit-file');
+const util = require('util');
+
+const directory = `${__dirname}/data/${process.argv[2]}`;
+
+
+let readfilePromise = util.promisify(file.read);
+let saveFilePromise = util.promisify(file.save);
+
+
+
+const fileRules = {
+  fields: {
+    firstName: {
+      type: 'string',
+      required: true,
+    },
+    lastName: {
+      type: 'string',
+      required: true,
+    },   
+    hair: {
+      type: 'array',
+      valueType: 'string',
+    },
+  },
+};
+
+readfilePromise(directory).then(data => {
+
+  data.firstName = 'David';
+  data.lastName = 'Vanderbeek';
+  data.married = true;
+  
+  saveFilePromise(data, directory, fileRules).then(stuff => console.log('Success')).catch(err => {
+    throw err;
+  });
+  
+});
+
+
+
+console.log(directory);
